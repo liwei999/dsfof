f59547643990a4dcbfae839c0c2c0f3a872a0779
liwei999 <lw@dsfof.com>
2018-03-16 09:55:06 +0800

    我删除 了

---
+++ project1/pages/index/index.js
@@ -0,0 +1,29 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../login/login'
+    })
+  },
+  onLoad: function () {
+    setTimeout(function(){
+      wx.redirectTo({
+        url: '../login/login',
+      })
+    },3000)
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
