25b095e7d0a1f5a5c927182b48cacea2537266f8
kun1937 <huangk@dsfof.com>
2018-03-20 18:15:37 +0800

    添加用户名与退出弹窗

--- project1/pages/setup/setup.js
+++ project1/pages/setup/setup.js
@@ -19,5 +19,25 @@ Page({
       title: '缓冲已清除',
       icon: 'success'
     })
+  },
+
+  unsubscribeEvent: function (e) {
+    var _this = this;
+    console.log(e);
+    var idx = e.currentTarget.dataset.idx;
+    wx.showModal({
+      title: '提示',
+      content: '确定退出当前账号登录？',
+      success: function (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+          wx.redirectTo({
+            url: '../login/login',
+          })
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
   }
 })
