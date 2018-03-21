a316ab4ce921d7034e4edcd81bffe55793cbf0ce
kun1937 <37401979+kun1937@users.noreply.github.com>
2018-03-21 17:58:15 +0800

    Delete banner1.png

--- project1/utils/util.js
+++ project1/utils/util.js
@@ -1,3 +1,4 @@
+//格式化时间
 const formatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
@@ -9,13 +10,27 @@ const formatTime = date => {
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
+
 const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
+
+//接口地址
 const urlstr = "https://vojs.dsfof.com.cn/";
+
+//暴露接口
 module.exports = {
   formatTime: formatTime,
-  urlstr: urlstr
+  urlstr: urlstr,
+  toast: toast
+}
+
+//显示提示
+function toast(toast) {
+  wx.showToast({
+    title: toast,
+    duration: 2000
+  })
 }
 
